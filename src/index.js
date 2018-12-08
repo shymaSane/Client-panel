970770a7a7846d8291c4ec3974d311703246a3b8
shymaSane <shymaSane1@gmail.com>
2018-12-08 11:36:19 +0200

    firebase config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyC_q921fwMQroeV1y90A-6ceWGjsFxYx3Y",
+    authDomain: "client-panel-3e604.firebaseapp.com",
+    databaseURL: "https://client-panel-3e604.firebaseio.com",
+    projectId: "client-panel-3e604",
+    storageBucket: "client-panel-3e604.appspot.com",
+    messagingSenderId: "81863483064"
+  };
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
