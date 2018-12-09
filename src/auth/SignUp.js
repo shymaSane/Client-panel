14515bb66eff215852422534244a511e0695c796
shymaSane <shymaSane1@gmail.com>
2018-12-09 12:38:32 +0200

    sign up bootstrap with some fix

--- src/auth/SignUp.js
+++ src/auth/SignUp.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class SignIn extends Component {
+class SignUp extends Component {
   render() {
     return (
     <div className="container d-flex justify-content-center mt-5">
@@ -12,12 +12,20 @@ class SignIn extends Component {
                 <div className="card-body">
                     <form>
                         <div className="form-group">
+                            <label htmlFor="name" className="text-white">Name</label>
+                            <input type="text" className="form-control" id="name" name="name" placeholder="Enter name"></input>
+                        </div>
+                        <div className="form-group">
                             <label htmlFor="email" className="text-white">Email address</label>
                             <input type="email" className="form-control" id="email" name="email" placeholder="Enter email"></input>
                         </div>
                         <div className="form-group">
                             <label htmlFor="password" className="text-white">password</label>
                             <input type="password" className="form-control" id="password" name="password" placeholder="Enter password"></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="confirm password" className="text-white">confirm password</label>
+                            <input type="password" className="form-control" id="password2" name="password2" placeholder="Confirm password"></input>
                         </div>
                         <button type="submit" className="btn btn-danger btn-block">Sign In</button>
                     </form>
@@ -29,4 +37,4 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn
+export default SignUp
--- src/layouts/Nav.js
+++ src/layouts/Nav.js
@@ -8,11 +8,11 @@ import './Nav.css'
         <a className="navbar-brand" href="#">
           <img src="/img/logo.png" alt="logo image" id="logo-img"></img>
         </a>
-        <button class="navbar-toggler btn btn-outline-danger mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon btn btn-outline-danger btn-sm"></span>
+        <button className="navbar-toggler btn btn-outline-danger mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon btn btn-outline-danger btn-sm"></span>
         </button>
   
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <a className="nav-link text-black " href="#"><button className="btn btn-sm btn-outline-danger">Sign In</button></a>
