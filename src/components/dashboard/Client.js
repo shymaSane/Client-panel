37501c1df3811db155b8b7639ba2ce8053fa57b6
shymaSane <shymaSane1@gmail.com>
2018-12-10 20:15:23 +0200

    destails button bootstrap

--- src/components/dashboard/Client.js
+++ src/components/dashboard/Client.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 
  class Client extends Component {
+   state = {
+     counter: 0,
+   }
+
+    Addcounter = () =>{
+      this.setState({
+        counter: 1
+      })
+   }
   render() {
     const {name, email, balance} = this.props
+  
     return (
         <tr>
-        <th scope="row">1</th>
+        <th scope="row">{this.state.counter}</th>
         <td>{name}</td>
         <td>{email}</td>
         <td>{balance}</td>
+        <td><button className="btn btn-secondary">Detailes</button></td>
         </tr>
 )
   }
