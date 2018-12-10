99e0736b8cc7535d0bc9a217ecb9dbce8e784953
shymaSane <shymaSane1@gmail.com>
2018-12-10 20:09:49 +0200

    fixed displaying tables

--- src/components/dashboard/Client.js
+++ src/components/dashboard/Client.js
@@ -4,17 +4,13 @@ import React, { Component } from 'react'
   render() {
     const {name, email, balance} = this.props
     return (
-      <div>
-         
-            
-                <tr>
-                <th scope="row">1</th>
-                <td>{name}</td>
-                <td>{email}</td>
-                <td>{balance}</td>
-                </tr>
-      </div>
-    )
+        <tr>
+        <th scope="row">1</th>
+        <td>{name}</td>
+        <td>{email}</td>
+        <td>{balance}</td>
+        </tr>
+)
   }
 }
 
