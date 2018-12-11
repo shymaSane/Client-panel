1419594d339c5964b6944dc9ea1dfafa2221877f
shymaSane <shymaSane1@gmail.com>
2018-12-11 14:11:40 +0200

    add get clients case

---
+++ src/reducers/ClientReducers.js
@@ -0,0 +1,15 @@
+import {GET_CLIENTS, ADD_CLIENT, DELETE_CLIENT, EDIT_CLIENT, UPDATE_CLIENT } from '../actions/types'
+
+const initialState = {
+    clients: []
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case GET_CLIENTS:
+            return {
+                ...state,
+                clients: action.payload
+            }
+    }
+}
