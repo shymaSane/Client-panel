3aaf610333a99684c79e577f624c2e2741c42e5e
shymaSane <shymaSane1@gmail.com>
2018-12-29 14:28:16 +0200

    router protection

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import NotFound from './components/info/NotFound';
 import Dashboard from './components/dashboard/Dashboard';
 import AddClient from './components/dashboard/AddClient';
 import Detailes from './components/dashboard/detailes/Detailes';
-
+import {UserIsAuthenticated, UserIsNotAuthenticated} from './helpers/protectRouting'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import store from './store'
 import {Provider} from 'react-redux'
@@ -25,13 +25,13 @@ class App extends Component {
             <div className="App">
               <Nav />
               <Switch>
-                <Route exact path='/' component={Welcome} />
-                <Route exact path='/users/signin' component={SignIn} />
-                <Route exact path='/users/signup' component={SignUp} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/dashboard' component={Dashboard} />
-                <Route exact path='/client/add' component={AddClient} />
-                <Route exact path='/client/detailes/:id' component={Detailes}/>
+                <Route exact path='/' component={UserIsNotAuthenticated(Welcome)} />
+                <Route exact path='/users/signin' component={UserIsNotAuthenticated(SignIn)} />
+                <Route exact path='/users/signup' component={UserIsNotAuthenticated(SignUp)} />
+                <Route exact path='/about' component={UserIsNotAuthenticated(About)} />
+                <Route exact path='/dashboard' component={UserIsAuthenticated(Dashboard)} />
+                <Route exact path='/client/add' component={UserIsAuthenticated(AddClient)} />
+                <Route exact path='/client/detailes/:id' component={UserIsAuthenticated(Detailes)}/>
                 <Route component={NotFound}/>
               </Switch>
             </div>
--- src/actions/types.js
+++
@@ -1,2 +0,0 @@
-// export const GET_CLIENTS = "GET_CLIENTS"
-// export const GET_CLIENT = "GET_CLIENT"
