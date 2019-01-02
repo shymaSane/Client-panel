5851670fb02bc70c2a3b2e6fa570fc55e48e14ed
shymaSane <shymaSane1@gmail.com>
2019-01-02 16:53:55 +0200

    setup settings component with state toggle

--- src/components/dashboard/Settings.js
+++ src/components/dashboard/Settings.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {disableBalance, disableRegister} from '../../actions/SettingsCreator'
+ 
+class Settings extends Component {
+    onAllowBalanceChange = () =>{
+        this.props.disableBalance()
+    }
 
-export default class Settings extends Component {
+    onAllowRegisterChange= () =>{
+        this.props.disableRegister()
+    }
   render() {
+      const {disable_balance, disable_register} = this.props.settings
     return (
     <div className="container d-flex justify-content-center">
-        <div className="col-sm-11 col-md-8 ">
-            <div className="card card-body">
-                <div className="form-check my-2">
-                    <input className="form-check-input" type="checkbox" value="" id="balance"></input>
-                        <label className="form-check-label-lg d-block" htmlFor="balance">
-                            Disable Balance
-                        </label>
-                </div>
-                <div className="form-check my-2">
-                    <input className="form-check-input" type="checkbox" value="" id="register"></input>
-                        <label className="form-check-label-lg d-block" htmlFor="register">
-                            Disable Register
-                        </label>
-                </div>
+        <div className="col-sm-11 col-md-8 card">
+        <div className="card-header">Change settings</div>
+            <div className="card-body">
+                <form>
+                    <div className="form-group my-2">
+                        <label htmlFor="balance">Disable Balance</label> {' '}   <input type="checkbox" name="disable_balance" checked={!!disable_balance} onChange={this.onAllowBalanceChange}></input>  
+                    </div>
+                    <div className="form-group my-2">
+                        <label htmlFor="register">Disable Register</label> {' '} <input  type="checkbox" name="disable_register" checked={!!disable_register} onChange={this.onAllowRegisterChange}></input>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -25,3 +32,8 @@ export default class Settings extends Component {
   }
 }
 
+export default connect((state, props) => ({
+    auth: state.firebase.auth,
+    settings: state.settings
+}),{disableRegister, disableBalance})
+(Settings)
